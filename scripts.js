a7b97d5d0a95efcf95649c7165f397d498b05f6c
JP Brunache <34013885+kosmik7@users.noreply.github.com>
2024-11-19 23:56:25 +0100

    Add the logic to win the game

--- scripts.js
+++ scripts.js
@@ -7,8 +7,9 @@ const gameBoard = (() => {
         console.log(`Player ${playerIndex} placed a mark in position ${pos}`, board)
         return true
     }
+    const resetBoard = () => board.fill(null);
 
-    return { placeMark }
+    return { placeMark, resetBoard }
 })();
 
 
@@ -17,22 +18,51 @@ const players = ((
     playerTwoName = "Player Two"
 ) => {
     const list = [
-        { name: playerOneName, id: 1 },
-        { name: playerTwoName, id: 2 }
+        { name: playerOneName, id: 1, board: [] },
+        { name: playerTwoName, id: 2, board: [] }
     ]
     let currentIndex = 0;
 
     const getCurrentPlayer = () => list[currentIndex];
+    const setPositions = (pos) => list[currentIndex].board.push(pos);
+    const getPositions = () => playerBoard;
     const nextTurn = () => currentIndex = 1 - currentIndex;
 
-    return { getCurrentPlayer, nextTurn }
+    return { getCurrentPlayer, nextTurn, setPositions, getPositions }
 })();
 
 
 const gameController = (function () {
+    let winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    const checkWinConditions = (currentPlayer) => {
+        const hasWinConditions = winConditions.some(
+            condition => condition.every(
+                pos => currentPlayer.board.includes(pos))
+        );
+        if (hasWinConditions) {
+            console.log(currentPlayer.name + " has won")
+            gameBoard.resetBoard()
+        }
+    }
+
     const playTurn = (position) => {
-        const play = gameBoard.placeMark(players.getCurrentPlayer().id, position)
-        if (play) players.nextTurn();
+        const currentPlayer = players.getCurrentPlayer()
+        const play = gameBoard.placeMark(currentPlayer.id, position)
+        if (play) {
+            players.setPositions(position)
+            checkWinConditions(currentPlayer)
+            players.nextTurn();
+        }
     }
 
     return { playTurn }
